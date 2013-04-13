4d1ccca9e8df1f555b644ef5ff25adb277de2a20
Nick Pack <nick@nickpack.com>
2013-04-13 21:28:23 +0100

    Meh

--- BlockCrap.java
+++ BlockCrap.java
@@ -12,13 +12,13 @@ public class BlockCrap extends Block {
 	
 	public BlockCrap(int i)
     {
-            super(i, Material.rock);
-            this.setCreativeTab(CreativeTabs.tabBlock);
+        super(i, Material.rock);
+        this.setCreativeTab(CreativeTabs.tabBlock);
     }
 
     public int idDropped(int i, Random random)
     {
-            return 0;
+        return 0;
     }
     
     public boolean onBlockActivated(World world, int i, int j, int k, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9)
@@ -27,47 +27,36 @@ public class BlockCrap extends Block {
     	int size = 10;
         for (int i1 = 0; i1 < size; i1++)
         {
-                for (int j1 = 0; j1 < size; j1++)
-                {
-                        //Walls 
-                        // i == ++ k == 0
-                        world.setBlock(i+i1,j+j1,k,block);
-                        // i == ++ k == 4
-                        world.setBlock(i+i1,j+j1,k+(size-1),block);
-                        // i == 0 k == ++
-                        world.setBlock(i,j+j1,k+i1,block);
-                        // i == 4 k == ++
-                        world.setBlock(i+(size-1),j+j1,k+i1,block);
-                        
-                        //Roof
-                        world.setBlock(i+i1,j+(size-1),k+j1,block);
-
-                        //Floor
-                        world.setBlock(i+i1,j,k+j1,block);
-                }
+            for (int j1 = 0; j1 < size; j1++)
+            {
+                world.setBlock(i+i1, j+j1, k, block);
+                world.setBlock(i+i1, j+j1, k+(size-1), block);
+                world.setBlock(i, j+j1, k+i1, block);
+                world.setBlock(i+(size-1), j+j1, k+i1, block);
+                world.setBlock(i+i1,j+(size-1),k+j1,block);
+                world.setBlock(i+i1,j,k+j1,block);
+            }
         }
-        //Door
-        world.setBlock(i+1,j+3,k,0);
-        world.setBlock(i+1,j+2,k,0);
-        world.setBlock(i+1,j+1,k,0);
-        world.setBlock(i+2,j+3,k,0);
-        world.setBlock(i+2,j+2,k,0);
-        world.setBlock(i+2,j+1,k,0);
+        
+        world.setBlock(i+1, j+3, k, 0);
+        world.setBlock(i+1, j+2, k, 0);
+        world.setBlock(i+1, j+1, k, 0);
+        world.setBlock(i+2, j+3, k, 0);
+        world.setBlock(i+2, j+2, k, 0);
+        world.setBlock(i+2, j+1, k, 0);
 
-        //Windows
         int offset = size % 2 == 0 ? (size / 2) - 1 : (size / 2);
-        world.setBlock(i,j+offset-2,k+offset,Block.glass.blockID);
-        world.setBlock(i,j+offset-2,k+offset+1,Block.glass.blockID);
-        world.setBlock(i+offset,j+offset-2,k,Block.glass.blockID);
-        world.setBlock(i+offset,j+offset-2,k+1,Block.glass.blockID);
-        world.setBlock(i+(size-1),j+offset-2,k+offset,Block.glass.blockID);
-        world.setBlock(i+offset,j+offset-2,k+(size-1),Block.glass.blockID);
+        world.setBlock(i, j+offset-2, k+offset, Block.glass.blockID);
+        world.setBlock(i, j+offset-2, k+offset+1, Block.glass.blockID);
+        world.setBlock(i+offset, j+offset-2, k, Block.glass.blockID);
+        world.setBlock(i+offset, j+offset-2, k+1, Block.glass.blockID);
+        world.setBlock(i+(size-1), j+offset-2, k+offset, Block.glass.blockID);
+        world.setBlock(i+offset, j+offset-2, k+(size-1), Block.glass.blockID);
 
-        //Utilities
-        world.setBlock(i+1,j+1,k+(size-2),Block.chest.blockID);
-        world.setBlock(i+(size-2),j+1,k+1,Block.workbench.blockID);
-        world.setBlock(i+(size/2),j+1,k+1,Block.glowStone.blockID);
-        world.setBlock(i+(size-2),j+1,k+(size-2),Block.furnaceIdle.blockID);
+        world.setBlock(i+1, j+1, k+(size-2), Block.chest.blockID);
+        world.setBlock(i+(size-2), j+1, k+1, Block.workbench.blockID);
+        world.setBlock(i+(size/2), j+1, k+1, Block.glowStone.blockID);
+        world.setBlock(i+(size-2), j+1, k+(size-2), Block.furnaceIdle.blockID);
         
         return true;
     }
